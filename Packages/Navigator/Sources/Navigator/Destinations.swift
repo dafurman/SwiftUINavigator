21fc1d838ed40075a01d42b6c4023647b3fb62c1
David Furman <dfurman@acorns.com>
2022-08-07 21:18:08 -0700

    Navigator: Added Destinations and Navigator

---
+++ Packages/Navigator/Sources/Navigator/Destinations.swift
@@ -0,0 +1,30 @@
+// Created 8/7/22
+
+import Foundation
+
+public protocol Destination: Hashable, Identifiable, Equatable, CaseIterable {
+    var rawValue: String { get }
+}
+
+public enum HomeDestination: String, Destination, Identifiable {
+    case home
+    case settings
+    public var id: String { rawValue }
+}
+
+public enum LoginDestination: String, Destination, Identifiable {
+    case emailPassword
+    case forgotPassword
+    public var id: String { rawValue }
+}
+
+public enum RegistrationDestination: String, Destination, Identifiable {
+    case newUser
+    public var id: String { rawValue }
+}
+
+public enum ModuleDestination {
+    case home(HomeDestination)
+    case login(LoginDestination)
+    case registration(RegistrationDestination)
+}
--- Packages/Navigator/Sources/Navigator/Navigator.swift
+++ Packages/Navigator/Sources/Navigator/Navigator.swift
@@ -1,6 +1,46 @@
-public struct Navigator {
-    public private(set) var text = "Hello, World!"
+// Created 8/7/22
 
-    public init() {
+import SwiftUI
+
+public final class Navigator: ObservableObject {
+
+    enum NavigatorError: Error {
+        case destinationNotInNavigationPath
+        case popToTop
     }
+
+    @Published public var path = NavigationPath() {
+        didSet {
+            if path.count < oldValue.count {
+                pathDestinations.removeLast(oldValue.count - path.count)
+            }
+        }
+    }
+    /// This must remain synchronized with `path`, in order to determine what `Destination`s are in the path.
+    private(set) public var pathDestinations: [any Destination] = []
+
+    // MARK: - Object Lifecycle
+
+    public init() {}
+
+    // MARK: - Navigation
+
+    public func navigate(to destination: any Destination) {
+        path.append(destination)
+        pathDestinations.append(destination)
+    }
+
+    public func pop(count: Int = 1) {
+        path.removeLast(count)
+        pathDestinations.removeLast(count)
+    }
+
+    public func pop(to destination: any Destination) throws {
+        guard let index = pathDestinations.lastIndex(where: { $0.rawValue == destination.rawValue }) else { throw NavigatorError.destinationNotInNavigationPath }
+        let countToPop = pathDestinations.count - 1 - index
+        guard countToPop > 0 else { throw NavigatorError.popToTop }
+
+        pop(count: countToPop)
+    }
+
 }
